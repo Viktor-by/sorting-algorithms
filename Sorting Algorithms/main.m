86b9d4d54a63e9466f7e25943663ddd7d058324f
Viktor Starovoitov <mail@viktor.by>
2015-11-21 11:42:57 +0300

    Initial Commit

---
+++ Sorting Algorithms/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sorting Algorithms
+//
+//  Created by Viktor Starovoitov on 11/21/15.
+//  Copyright © 2015 Viktor Starovoitov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
